ac96c3461b787a63f64d54414e23dd1652ce00f3
Sam Salisbury <samsalisbury@gmail.com>
2015-03-26 14:21:06 +0000

    Add bootstrap script for singularity

---
+++ run-singularity.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+die() { echo $1 1>&2; exit 1; }
+if ! command -v pip >/dev/null; then
+	die "pip required"
+fi
+if ! (pip list | grep requests); then
+	pip install requests
+fi
+./mesos-stats-graphite.py $@
