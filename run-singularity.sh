2add6aa874b59ed7ed06ea8ee9eefb441d71ed8d
Sam Salisbury <samsalisbury@gmail.com>
2015-03-29 00:52:44 +0000

    Delete old files

--- run-singularity.sh
+++ run-singularity.sh
@@ -3,7 +3,7 @@ die() { echo $1 1>&2; exit 1; }
 if ! command -v pip >/dev/null; then
 	die "pip required"
 fi
-if ! (pip list | grep requests); then
-	pip install requests
+if [ -f requirements.txt ]; then
+	pip install -r requirements.txt
 fi
 ./mesos-stats-graphite.py $@
