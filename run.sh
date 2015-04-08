c39f254fbff97f0461644290ad045899812bc0ca
Sam Salisbury <samsalisbury@gmail.com>
2015-04-08 12:28:27 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-die() { echo $1 1>&2; exit 1; }
-if ! command -v pip >/dev/null; then
-	die "pip required"
-fi
-if ! (pip list | grep requests); then
-	pip install requests
-fi
-"$@"
