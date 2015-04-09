6c11108039d56ac9dc95be74461115d5e43d2ad7
Sam Salisbury <samsalisbury@gmail.com>
2015-04-09 15:07:06 +0100

    Finished refactoring

--- mesos_stats/mesos.py
+++ mesos_stats/mesos.py
@@ -4,101 +4,119 @@ import requests
 
 class Mesos:
     def __init__(self, master_pid):
-        self.master_pid = master_pid
         self.leader_pid = master_pid
+        self.leader_state = None
+        self.slave_states = None
 
-    def get_slave_stats(self, slave_pid):
-        return try_get_json("http://%s/metrics/snapshot" % slave_pid)
-    
     def get_master_state(self):
-        url = "http://%s/state.json" % self.master_pid
+        url = "http://%s/state.json" % self.leader_pid
         log("Getting master state from: %s" % url)
         return try_get_json(url)
     
-    def get_leader_state(self):
+    def state(self):
+        if self.leader_state != None:
+            return self.leader_state
         master = self.get_master_state()
         if master == None:
             return None
         if master["pid"] == master["leader"]:
-            return master
-        self.master_pid = master["pid"]
-        return self.get_leader_state()
-    
-    def get_cluster_metrics(self):
-        leader = self.get_leader_state()
-        if leader == None:
-            raise Exception("No leader found.")
+            self.leader_state = master
+        else:
+            self.leader_pid = master["leader"]
+        return self.state()
 
-        clusterMetrics = self.makeClusterMetrics()
-        url = "http://%s/metrics/snapshot" % self.leader_pid
-        cluster = try_get_json(url)
-        for m in clusterMetrics:
-            m.Add(cluster)
+    def get_slave(self, slave_pid):
+        return try_get_json("http://%s/metrics/snapshot" % slave_pid)
+
+    def get_slave_statistics(self, slave_pid):
+        return try_get_json("http://%s/monitor/statistics" % slave_pid)
     
-        slaveMetrics = self.makeSlaveMetrics()
-        for s in leader["slaves"]:
-            log("Getting stats for %s" % s["pid"])
-            slave = self.get_slave_stats(s["pid"])
+    def slaves(self):
+        if self.slave_states != None:
+            return self.slave_states
+        self.slave_states = {}
+        for slave_pid in self.state()["slaves"]:
+            log("Getting stats for %s" % slave_pid)
+            slave = self.get_slave(slave_pid)
             if slave == None:
-                log("Slave lost")
+                log("Slave lost: %s" % slave_pid)
                 continue
-            for m in slaveMetrics:
-                m.Add(slave)
+            slave_statistics = self.get_slave_statistics(slave_pid)
+            self.slave_states[slave_pid] = self.get_slave(slave_pid)
+            self.slave_states[slave_pid]["statistics"] = slave_statistics 
 
-        allMetrics = clusterMetrics + slaveMetrics
+        return self.slave_states
 
-        return allMetrics
-    
-        #try:
-        #    send_metrics(allMetrics)
-        #except socket.error:
-        #    log("WARNING: Unable to send pickled stats on port 2004; Attempting plaintext (slow) on 2003...")
-        #    try:
-        #        send_metrics_slow(allMetrics)
-        #    except socket.error:
-        #        log("ERROR: Unable to send plantext on port 2003")
-    
-        log("Metrics sent.")
-        return leader["pid"]
-        
-    def makeSlaveMetrics(self):
-        return [
-            Metric("slave/mem_total",        "slave.[].mem.total",               Each()),
-            Metric("slave/mem_used",         "slave.[].mem.used",                Each()),
-            Metric("slave/mem_percent",      "slave.[].mem.percent",             Each(scale=100)),
-            Metric("slave/cpus_total",       "slave.[].cpus.total",              Each()),
-            Metric("slave/cpus_used",        "slave.[].cpus.used",               Each()),
-            Metric("slave/cpus_percent",     "slave.[].cpus.percent",            Each(scale=100)),
-            Metric("slave/disk_total",       "slave.[].disk.total",              Each()),
-            Metric("slave/disk_used",        "slave.[].disk.total",              Each()),
-            Metric("slave/tasks_running",    "slave.[].tasks.running",           Each()),
-            Metric("slave/tasks_staging",    "slave.[].tasks.staging",           Each()),
-            Metric("system/load_1min",       "slave.[].system.load.1min",        Each(scale=1000)),
-            Metric("system/load_5min",       "slave.[].system.load.5min",        Each(scale=1000)),
-            Metric("system/load_15min",      "slave.[].system.load.15min",       Each(scale=1000)),
-            Metric("system/mem_free_bytes",  "slave.[].system.mem.free.bytes",   Each()),
-            Metric("system/mem_total_bytes", "slave.[].system.mem.total.bytes",  Each()),
+def slave_metrics(mesos):
+    metrics = [
+        Metric("slave/mem_total",        "slave.[].mem.total",               Each()),
+        Metric("slave/mem_used",         "slave.[].mem.used",                Each()),
+        Metric("slave/mem_percent",      "slave.[].mem.percent",             Each(scale=100)),
+        Metric("slave/cpus_total",       "slave.[].cpus.total",              Each()),
+        Metric("slave/cpus_used",        "slave.[].cpus.used",               Each()),
+        Metric("slave/cpus_percent",     "slave.[].cpus.percent",            Each(scale=100)),
+        Metric("slave/disk_total",       "slave.[].disk.total",              Each()),
+        Metric("slave/disk_used",        "slave.[].disk.total",              Each()),
+        Metric("slave/tasks_running",    "slave.[].tasks.running",           Each()),
+        Metric("slave/tasks_staging",    "slave.[].tasks.staging",           Each()),
+        Metric("system/load_1min",       "slave.[].system.load.1min",        Each(scale=1000)),
+        Metric("system/load_5min",       "slave.[].system.load.5min",        Each(scale=1000)),
+        Metric("system/load_15min",      "slave.[].system.load.15min",       Each(scale=1000)),
+        Metric("system/mem_free_bytes",  "slave.[].system.mem.free.bytes",   Each()),
+        Metric("system/mem_total_bytes", "slave.[].system.mem.total.bytes",  Each()),
+    ]
+
+    for pid, s in mesos.slaves():
+        for m in metrics:
+            m.Add(slave)
+
+    return metrics
+
+def slave_singluarity_task_metrics(mesos):
+    ms = []
+    for pid, slave in mesos.Slaves():
+        prefix = "slave.%s.executors.singularity.tasks.[]" % pid
+        metrics = [
+            Metric("cpus_system_time_secs", prefix + ".cpus.system_time_secs", Each()),
+            Metric("cpus_user_time_secs",   prefix + ".cpus.user_time_secs",   Each()),
+            Metric("cpus_limit",            prefix + ".cpus.limit",            Each()),
+            Metric("mem_limit_bytes",       prefix + ".mem.limit_bytes",       Each()),
+            Metric("mem_rss_bytes",         prefix + ".mem.rss_bytes",         Each()),
         ]
-    
-    def makeClusterMetrics(self):
-        return [
-            Metric("master/cpus_percent",     "cluster.cpus.percent",     Each(scale=100)),
-            Metric("master/cpus_total",       "cluster.cpus.total",       Each()),
-            Metric("master/cpus_used",        "cluster.cpus.used",        Each()),
-            Metric("master/mem_percent",      "cluster.mem.percent",      Each(scale=100)),
-            Metric("master/mem_total",        "cluster.mem.total",        Each()),
-            Metric("master/mem_used",         "cluster.mem.used",         Each()),
-            Metric("master/disk_percent",     "cluster.disk.percent",     Each(scale=100)),
-            Metric("master/disk_total",       "cluster.disk.total",       Each()),
-            Metric("master/disk_used",        "cluster.disk.used",        Each()),
-            Metric("master/slaves_connected", "cluster.slaves.connected", Each()),
-            Metric("master/tasks_failed",     "cluster.tasks.failed",     Each()),
-            Metric("master/tasks_finished",   "cluster.tasks.finished",   Each()),
-            Metric("master/tasks_killed",     "cluster.tasks.killed",     Each()),
-            Metric("master/tasks_lost",       "cluster.tasks.lost",       Each()),
-            Metric("master/tasks_running",    "cluster.tasks.running",    Each()),
-            Metric("master/tasks_staging",    "cluster.tasks.staging",    Each()),
-            Metric("master/tasks_starting",   "cluster.tasks.starting",   Each()),
-        ]
-    
+        for m in metrics:
+            for t in slave.tasks:
+                m.Add(t)
+
+        ms += metrics
+
+    return ms
+
+
+def cluster_metrics(mesos):
+    metrics = [
+        Metric("master/cpus_percent",     "cluster.cpus.percent",     Each(scale=100)),
+        Metric("master/cpus_total",       "cluster.cpus.total",       Each()),
+        Metric("master/cpus_used",        "cluster.cpus.used",        Each()),
+        Metric("master/mem_percent",      "cluster.mem.percent",      Each(scale=100)),
+        Metric("master/mem_total",        "cluster.mem.total",        Each()),
+        Metric("master/mem_used",         "cluster.mem.used",         Each()),
+        Metric("master/disk_percent",     "cluster.disk.percent",     Each(scale=100)),
+        Metric("master/disk_total",       "cluster.disk.total",       Each()),
+        Metric("master/disk_used",        "cluster.disk.used",        Each()),
+        Metric("master/slaves_connected", "cluster.slaves.connected", Each()),
+        Metric("master/tasks_failed",     "cluster.tasks.failed",     Each()),
+        Metric("master/tasks_finished",   "cluster.tasks.finished",   Each()),
+        Metric("master/tasks_killed",     "cluster.tasks.killed",     Each()),
+        Metric("master/tasks_lost",       "cluster.tasks.lost",       Each()),
+        Metric("master/tasks_running",    "cluster.tasks.running",    Each()),
+        Metric("master/tasks_staging",    "cluster.tasks.staging",    Each()),
+        Metric("master/tasks_starting",   "cluster.tasks.starting",   Each()),
+    ]
+
+    for m in metrics:
+        m.Add(mesos.state())
+
+    return metrics
+   
+
     
