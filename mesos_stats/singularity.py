dbda874240d1d75e71753d26d29a1142f69fb8a0
Sam Salisbury <samsalisbury@gmail.com>
2015-05-18 18:13:51 +0100

    Singularity overdue tasks heuristic
    - OVERDUE status does not appear in deploy state for some reason
    - Instead, report overdue tasks as those whose "next run" timestamp
      is in the past.

--- mesos_stats/singularity.py
+++ mesos_stats/singularity.py
@@ -1,5 +1,6 @@
 from metric import Metric, Each
 from util import log, try_get_json
+import time
 
 class Singularity:
     def __init__(self, host):
@@ -14,6 +15,14 @@ class Singularity:
 
     def pending_deploys(self):
         return self.__get("/deploys/pending")
+
+    # scheduled_tasks should nod be confused with the common concept of scheduled
+    # tasks. This indicates any task which is scheduled to be run, including tasks
+    # which have been invoked to run immediately, and long-running services
+    # or workers. All of these task types become a scheduled task prior to being
+    # run on a slave.
+    def scheduled_tasks(self):
+        return self.__get("/tasks/scheduled")
 
     def __get(self, uri):
         if self.__cache.get(uri) == None:
@@ -33,8 +42,20 @@ class DigestedMetric:
         return [(self.key, self.value)]
 
 def singularity_metrics(singularity):
+    # TODO: Figure out why deploy state is never OVERDUE, even when the deploy is
+    # overdue.
     pending_deploys = singularity.pending_deploys()
-    overdue_deploys = [d for d in pending_deploys if d["currentDeployState"] == "OVERDUE"]
+    #overdue_deploys = [d for d in pending_deploys if d["currentDeployState"] == "OVERDUE"]
+
+    # Alternative method to determine overdue tasks: look at scheduled start time
+    # and set to overdue if it is in the past. This is how the Singularity UI
+    # calculates it.
+    scheduled_tasks = singularity.scheduled_tasks()
+    overdue_tasks = [t for t in scheduled_tasks if
+            int(t["pendingTask"]["pendingTaskId"]["nextRunAt"]) <
+            int(round(time.time() * 1000))]
+
+    overdue_deploys = overdue_tasks
     digested_metrics = [
         DigestedMetric("singularity.deploys.pending.total", len(pending_deploys)),
         DigestedMetric("singularity.deploys.pending.overdue", len(overdue_deploys)),
