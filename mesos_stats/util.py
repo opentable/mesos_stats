7c77d988119a998557e992cc9d2d73e2a9176129
Jay Chin <jayc@opentable.com>
2017-04-03 18:08:37 +0100

    Added tests and modifications to util

--- mesos_stats/util.py
+++ mesos_stats/util.py
@@ -1,31 +1,40 @@
 import json
 import time
+import sys
 import requests
 
-def try_get_json(url):
-    with timer("GET %s" % url):     
+def try_get_json(url, timeout=20):
+    with Timer("GET %s" % url):
         t = time.time()
         try:
-            return json.loads(requests.get(url, timeout=20).text)
+            response = requests.get(url, timeout=timeout)
         except requests.exceptions.Timeout:
             log("GET %s timed out after %s." % (url, time.time()-t))
+            raise
+        except requests.exceptions.MissingSchema:
+            log("%s is not a valid URL" % url)
             raise
         except requests.exceptions.ConnectionError as e:
             log("GET %s failed: %s" % (url, e))
             raise
-        except object as e:
-            log("GET %s returned a '%s' exception: %s" % (url, type(e), e))
+        except:
+            log("Unexpected error from %s : %s" % (url, sys.exc_info()[0]))
             raise
-   
+
+        if response.status_code == 200:
+            return json.loads(response.text)
+        else:
+            log("GET %s failed - Non 200 HTTP Error" % url)
+            return False
+
 def log(message):
     ts = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
     print('%s %s' % (ts, message))
 
-class timer:
+class Timer:
     def __init__(self, name):
         self.name = name
     def __enter__(self):
         self.time = time.time()
     def __exit__(self, type, value, traceback):
         return log("%s took %ss" % (self.name, time.time() - self.time))
-
